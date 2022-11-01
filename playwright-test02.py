cf736bb062a8ec7c42a225e77250cbd99c18099f
Sze Chan <sze@hello-sze.com>
2022-10-31 22:32:47 -0400

    Latest changes

--- playwright-test02.py
+++ playwright-test02.py
@@ -1,4 +1,6 @@
 import asyncio
+from bs4 import BeautifulSoup
+from lxml import etree
 from playwright.async_api import async_playwright
 
 async def main():
@@ -7,6 +9,16 @@ async def main():
         page = await browser.new_page()
         await page.goto("https://hondaeastcincy.com/honda-models/")
         print(await page.title())
+        # print(await page.content())
+        html = await page.content()
+
+        print(html)
+
+        soup = BeautifulSoup(html, 'html.parser')
+        print(soup.find_all('h3'))
+        for child in soup.select('h3.margin-top-none'):
+            print(child.contents)
+
         await browser.close()
 
 asyncio.run(main())
